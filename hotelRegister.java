2fdd5135ecdb66bc00143cfdcca33f1c96096745
briananhvu-tran <54463365+briananhvu-tran@users.noreply.github.com>
2020-11-22 23:21:49 -0800

    Update hotelRegister.java

--- hotelRegister.java
+++ hotelRegister.java
@@ -2,7 +2,9 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -46,6 +48,10 @@ public class hotelRegister extends HttpServlet {
         // get username and password input from register.jsp
         String username = request.getParameter("username");
         String upassword = request.getParameter("password");
+        String name = request.getParameter("name");
+        String phone = request.getParameter("phone");
+        String address = request.getParameter("address");
+        String email = request.getParameter("email");
           
         Connection conn = null; 
         String message = null;  // error message
@@ -53,16 +59,61 @@ public class hotelRegister extends HttpServlet {
         try {
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cs157a?serverTimezone=EST5EDT",user, password);
-            String sql = "INSERT INTO proj1test.account values (?, ?)";
+            String sql = "INSERT INTO proj1test.accounts (username, password, PermissionType) values (?, ?, ?)";
 
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setString(1, username);
             statement.setString(2, upassword);
+            statement.setString(3, "guest");
  
             int row = statement.executeUpdate();
             if (row > 0) {
                 message = "Registered successfully.";
             }
+            
+            sql = "INSERT INTO proj1test.guests (name, phone, Address, email) values (?, ?, ?, ?)";
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, name);
+            statement.setString(2, phone);
+            statement.setString(3, address);
+            statement.setString(4, email);
+            
+            row = statement.executeUpdate();
+            
+            sql = "SELECT AccountID FROM proj1test.accounts WHERE username = '" + username + "'";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            int accID = 0;
+            while (rs.next())
+            {
+            	accID = rs.getInt("AccountID");
+            }
+            
+            sql = "SELECT guestID FROM proj1test.guests WHERE email = '" + email + "'";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            
+            int guestID = 0;
+            while (rs.next())
+            {
+            	guestID = rs.getInt("guestID");
+            }
+            
+            sql = "INSERT INTO proj1test.register (accountID, guestID) values (?, ?)";
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, Integer.toString(accID));
+            statement.setString(2, Integer.toString(guestID));
+            
+            row = statement.executeUpdate();
+            
+            sql = "INSERT INTO proj1test.registered (accountID, PaymentInfo) values (?, ?)";
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, accID);
+            statement.setString(2, "credit card");
+            
+            row = statement.executeUpdate();
+            
         } catch (SQLException ex) {
             message = "ERROR: " + ex.getMessage();
             ex.printStackTrace();
